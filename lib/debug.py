b79a7f0f8579ca05492c6dd9a674f98c5cbbca7d
Jazper <jazpersaldana@yahoo.com>
2024-06-19 14:45:42 -0700

    orm lab complete

--- lib/debug.py
+++ lib/debug.py
@@ -18,14 +18,13 @@ def reset_database():
 
     # Create seed data
     payroll = Department.create("Payroll", "Building A, 5th Floor")
-    human_resources = Department.create(
-        "Human Resources", "Building C, East Wing")
+    human_resources = Department.create("Human Resources", "Building C, East Wing")
     employee1 = Employee.create("Lee", "Manager", payroll.id)
     employee2 = Employee.create("Sasha", "Manager", human_resources.id)
     Review.create(2023, "Efficient worker", employee1.id)
     Review.create(2022, "Good work ethic", employee1.id)
     Review.create(2023, "Excellent communication skills", employee2.id)
 
-
 reset_database()
+
 ipdb.set_trace()
